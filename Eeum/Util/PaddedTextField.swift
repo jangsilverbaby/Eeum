7ccf25605c2e4a309807291b9652fd6ad232072f
eunaejang <dmsdo1234@gmail.com>
2022-10-16 08:58:51 +0900

    PaddedTextField 추가

---
+++ Eeum/Util/PaddedTextField.swift
@@ -0,0 +1,21 @@
+//
+//  PaddedTextField.swift
+//  Eeum
+//
+//  Created by eunae on 2022/10/16.
+//
+
+import UIKit
+
+class PaddedTextField: UITextField {
+    let padding = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 5)
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
